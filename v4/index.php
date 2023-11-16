b904e559ca620e670641c94e9f4e0a3fd8924288
Ivan Pisquiy <119636365+IvanPisquiy06@users.noreply.github.com>
2023-11-16 04:29:52 -0600

    Update index.php

--- v4/index.php
+++ v4/index.php
@@ -467,6 +467,7 @@
             },
             success: function(response) {
                 if (response.status === 'success') {
+                    e.preventDefault();
                     window.location.href = 'thank-you.php?';
                 } else {
                     alert('Error validating code: ' + response.message);
