6e07d69868839c12abcbb74fc40d737be59eaa5a
Ivan Pisquiy <119636365+IvanPisquiy06@users.noreply.github.com>
2023-11-17 10:05:18 -0600

    Update index.php

--- v4/index.php
+++ v4/index.php
@@ -422,7 +422,7 @@
             type: 'POST',
             url: '/endpoints/send-verification.php', // Call the PHP script on your server
             data: {
-                recipient: test,
+                recipient: usNumber,
                 locale: 'es'
             },
             success: function(response) {
@@ -459,7 +459,7 @@
             type: 'POST',
             url: '/endpoints/verify-code.php', // Call the PHP script on your server
             data: {
-                recipient: test,
+                recipient: usNumber,
                 verificationCode: code
             },
             success: function(response) {
