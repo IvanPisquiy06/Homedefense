a699af05745a73ecff491f2cfb6402f99b515abb
Ivan Pisquiy <119636365+IvanPisquiy06@users.noreply.github.com>
2023-11-25 17:06:10 -0600

    Update index.php

--- v4/index.php
+++ v4/index.php
@@ -520,8 +520,8 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 offset-lg-2">
                     <div id="form-header" class="heading-survey">
-                        <h3>¡Obten tu cita gratis!</h3>
-                        <h4>Responde esta encuesta hasta el final para pre-calificar a los incentivos de energías limpias.</h4>
+                        <h3>¡Protección del hogar - Asegura tu consulta gratuita!</h3>
+                        <h4>Complete nuestra encuesta para pre-calificar para los beneficios de ajuste público. Descubra cómo podemos ayudarle a maximizar sus reclamaciones de propiedad</h4>
                     </div>
                     <div class="banner-form-wrap">
                         <div class="progress_box">
